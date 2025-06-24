c7d0982bf848693f71e37db28a011d303001029c
shiras <xxx@example.com>
2025-06-24 16:23:49 +0900

    Refactor AssignedTaskSelector: remove unused UnassignedTaskSelector and related commented code for improved clarity

--- src/app/models/AssignedTaskSelector.ts
+++ src/app/models/AssignedTaskSelector.ts
@@ -1,48 +1,13 @@
 import { AssignedTask } from "./AssignedTask";
 import { PlanedTask } from "./PlanedTask";
 import { TaskManager } from "./TaskManager";
-import { UnassignedTaskSelector } from "./UnassignedTaskSelector";
 import { PhaseEnum } from "../common/PhaseEnum";
 import { TaskResolver } from "./TaskResolver";
 import { UUID } from "../common/IdUtil";
 
 export class AssignedTaskSelector {
 
-    private _unassignedTaskSelector = new UnassignedTaskSelector();
-
     private _taskResolver = new TaskResolver();
-
-    /**
-     * 割り当て済みのタスクのうち、本来は割り当てられないはずのタスクを取得します。
-     * 割り当てられないかどうかの判定は別policyに委ねられています。
-     * @param taskManager 
-     * @param planedTask 
-     * @returns 
-     */
-    // public getUnassignableTasks(
-    //     taskManager: TaskManager,
-    //     planedTask: PlanedTask
-    // ): AssignedTask[] {
-
-    //     //TODO: 未実装
-    //     throw new Error("getUnassignableTasks is not implemented yet");
-
-    //     const prePhases = previousPhases(phase);
-    //     const aftPhases = [phase, ...afterPhases(phase)];
-    //     const unassignedPreTasks = this._unassignedTaskSelector.getUnassignedTaskFromTicketIdAndPhases(ticketId, prePhases, taskManager, planedTask);
-
-    //     const afterPhaseAssigned = planedTask.getFromTicketId(ticketId).filter(assignedTask => {
-    //         const resolvePhase = this._taskResolver.resolveAssignedTaskPhase(assignedTask.taskId, taskManager, planedTask);
-    //         return aftPhases.includes(resolvePhase);
-    //     })
-
-    //     if (unassignedPreTasks.length > 0) {
-    //         return afterPhaseAssigned;
-    //     }
-
-    //     return []
-
-    // }
 
     /**
      * 割り当て済みのタスクとそのフェーズを取得します。  
